e68fee8dbd8ad35b848a313c17870b43b93aa7d8
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-15 21:26:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,6 +17,11 @@ class Artist
   def self.all
     @@artists
   end
+  
+  def self.find_by_name(name)
+    to_param.detect {|a| a.name == name}
+    end
+  end
 
   def add_song(song)
     @songs << song
